d24bf0297f7b73f0d8b518de705bc7b45d070169
Namik Catibusic <namik.catibusic@klika.ba>
2014-12-14 15:41:27 +0100

    UI changeS

--- app/controllers/sphero_controller.rb
+++ app/controllers/sphero_controller.rb
@@ -2,15 +2,18 @@ class SpheroController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    get_client.rgb 100,100,100
   end
 
   def action
     puts get_client.response_queue.to_yaml
     get_client.send params[:perform], *params[:with]
+
     render json: {}, location: "/"  
   end
 
-  def get_client
-    @client ||= Sphero.new "/dev/tty.Sphero-OBP-AMP-SPP"
-  end
+  private
+    def get_client
+      @client ||= Sphero.new "/dev/tty.Sphero-OBP-AMP-SPP"
+    end
 end
