84cde2474308eb3c214a1299a0f6daa3fbd25fa5
Muamer Ribica <muamer.ribica@gmail.com>
2014-12-16 00:25:06 +0100

    Add rainbow

--- app/controllers/sphero_controller.rb
+++ app/controllers/sphero_controller.rb
@@ -1,21 +1,28 @@
 class SpheroController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  ALLOWED = ["rgb", "roll"]
+
   def index
   end
 
-  def info
-    render json: {
-      version: get_client.version,
-      bluetooth: get_client.bluetooth_info,
-      auto_reconnect: get_client.auto_reconnect,
-      power: get_client.power_state
-    }
+  def action
+    unless ALLOWED.include?(params[:perform])
+      head :unprocessable_entity
+    else
+      get_client.send params[:perform], *params[:with]
+      get_client.keep_going 1
+      head :ok
+    end
   end
 
-  def action
-    get_client.send params[:perform], *params[:with]
-    get_client.keep_going 2
+  def rainbow
+    colors = [127, 0, 255]
+
+    13.times do
+      get_client.rgb colors.sample, colors.sample, colors.sample
+      get_client.keep_going 0.2
+    end
 
     head :ok
   end
